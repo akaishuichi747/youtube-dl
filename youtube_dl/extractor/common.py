ff9ec08986aacbc63d8869a5e6702ae2b86cdd23
akaishuichi747 <54711211+akaishuichi747@users.noreply.github.com>
2019-09-15 09:01:22 -0400

    Update common.py

--- youtube_dl/extractor/common.py
+++ youtube_dl/extractor/common.py
@@ -2353,10 +2353,10 @@ class InfoExtractor(object):
             stream_timescale = int_or_none(stream.get('TimeScale')) or timescale
             stream_name = stream.get('Name')
             for track in stream.findall('QualityLevel'):
-                fourcc = 'HEV1'
-                # TODO: add support for WVC1 and WMAP
-                #if fourcc not in ('H264', 'AVC1', 'AACL', 'HEV1', 'EC-3', 'AACH'):
-                    #self.report_warning('%s is not a supported codec' % fourcc)
+                fourcc = track.get('FourCC', 'AACL' if track.get('AudioTag') == '255' else None)
+                 #TODO: add support for WVC1 and WMAP
+                if fourcc not in ('H264', 'AVC1', 'AACL', 'hev1', 'EC-3', 'AACH'):
+                    self.report_warning('%s is not a supported codec' % fourcc)
                 continue
                 tbr = int(track.attrib['Bitrate']) // 1000
                 # [1] does not mention Width and Height attributes. However,
